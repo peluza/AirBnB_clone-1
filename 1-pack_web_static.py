0aa24a2f2bd800b5b2c14ad6bc9c21361193ed74
peluza <edisonisaza@gmail.com>
2020-08-17 20:53:20 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-from fabric.api import *
-from datetime import datetime
+import fabric
+import datetime
 import os
 
 
 def do_pack():
-    local("mkdir -p versions")
+    fabric.api.local("mkdir -p versions")
     name_file = "versions/web_static_{:s}.tgz".format(
-        datetime.now().strftime("%Y%m%d%H%M%S"))
-    create_file = local("tar -cvzf {:s} web_static".format(name_file))
+        datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    create_file = fabric.api.local(
+        "tar -cvzf {:s} web_static".format(name_file))
     if os.path.isfile(name_file):
-        print(name_file)
+        print("web_static packed: {:s} -> {}Bytes".format(
+            name_file, os.path.getsize(name_file)))
+        return name_file
     else:
         return None
