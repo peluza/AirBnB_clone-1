d32c29557dcb2723ea08a153a9b9fae0863dbdd3
peluza <edisonisaza@gmail.com>
2020-08-17 21:07:26 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,8 +16,7 @@ def do_pack():
     local("mkdir -p versions")
     name_file = "versions/web_static_{:s}.tgz".format(
         datetime.now().strftime("%Y%m%d%H%M%S"))
-    create_file = local(
-        "tar -cvzf {:s} web_static".format(name_file))
+    local("tar -cvzf {:s} web_static".format(name_file))
     if os.path.isfile(name_file):
         print("web_static packed: {:s} -> {}Bytes".format(
             name_file, os.path.getsize(name_file)))
