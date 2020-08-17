39dbe6083ab410b74a703fd88ab2753707498291
peluza <edisonisaza@gmail.com>
2020-08-17 18:49:57 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#sets up your web servers for the deployment of web_static 
 if ! which nginx > /dev/null 2>&1; then
     apt-get update
     apt-get -y install nginx
@@ -8,7 +9,7 @@ mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "Holberton School for the win!" | tee /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/current /data/web_static/releases/test/
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 sed -i "45 a \ \n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
