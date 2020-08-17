8bf76eaf4fa4d8139e0d0ff39eef5756224137d8
peluza <edisonisaza@gmail.com>
2020-08-17 21:10:04 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@
 from fabric.api import local
 from fabric.decorators import runs_once
 from datetime import datetime
-import os
+from os.path import isfile, getsize
 
 
 @runs_once
@@ -17,9 +17,9 @@ def do_pack():
     name_file = "versions/web_static_{:s}.tgz".format(
         datetime.now().strftime("%Y%m%d%H%M%S"))
     local("tar -cvzf {:s} web_static".format(name_file))
-    if os.path.isfile(name_file):
+    if isfile(name_file):
         print("web_static packed: {:s} -> {}Bytes".format(
-            name_file, os.path.getsize(name_file)))
+            name_file, getsize(name_file)))
         return name_file
     else:
         return None
