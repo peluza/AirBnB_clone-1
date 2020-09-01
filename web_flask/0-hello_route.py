deaaf9463e591fe147884dbe6ef9d1c5d9e1957a
peluza <edisonisaza@gmail.com>
2020-09-01 14:00:29 -0500

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""starts a Flask web application:
+        must be listening on 0.0.0.0, port 5000
+    """
 
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
