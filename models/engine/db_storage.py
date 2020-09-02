a4ea64a0d7586970c52c1143724f9431c1eee6ed
peluza <edisonisaza@gmail.com>
2020-09-02 23:51:13 +0000

    AirBnB

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import MetaData
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 class DBStorage:
@@ -25,3 +26,36 @@ class DBStorage:
     def all(self, cls=None):
         class_list = [User, State, City, Amenity, Place, Review]
         all_dict = {}
+        if cls is not None:
+            if type(cls) == str:
+                class_obj = eval(cls)
+            else:
+                class_obj = cls
+                cls = str(cls)
+            for item in self.__session.query(class_obj):
+                all_dict[cls + "." + item.id] = item
+        else:
+            for table in class_list:
+                for item in self.__session.query(table):
+                    all_dict[table.__class__.__name__ + "." + item.id] = item
+        return all_dict
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+            self.__session.commit()
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(factory)
+
+    def close(self):
+        """call remove method"""
+        self.__session.remove()
